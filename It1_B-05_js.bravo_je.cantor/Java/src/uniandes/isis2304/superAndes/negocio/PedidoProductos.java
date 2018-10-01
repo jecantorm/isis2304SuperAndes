020c99fcaff897dbb6a7b54f2bafe16ddc8aab00
jsbravo-sw <js.bravo@uniandes.edu.co>
2018-10-01 00:03:10 -0500

    Borradas carpetas merge

---
+++ It1_B-05_js.bravo_je.cantor/Java/src/uniandes/isis2304/superAndes/negocio/PedidoProductos.java
@@ -0,0 +1,13 @@
+package uniandes.isis2304.superAndes.negocio;
+
+public class PedidoProductos {
+	
+	private String id;
+	
+	private String nombreSucursal;
+	
+	private String idProducto;
+	
+	private String nombreProveedor;
+
+}
---
+++ It1_B-05_js.bravo_je.cantor/Java/src/uniandes/isis2304/superAndes/negocio/ProveedorProductos.java
@@ -0,0 +1,13 @@
+package uniandes.isis2304.superAndes.negocio;
+
+public class ProveedorProductos {
+	
+	private String id;
+	
+	private String nombreProveedor;
+	
+	private String idProducto;
+	
+	private String nombrePromocion;
+
+}

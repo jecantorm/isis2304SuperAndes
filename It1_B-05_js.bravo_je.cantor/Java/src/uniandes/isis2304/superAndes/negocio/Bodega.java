d2131b68e0b207af9e9061ca2fb79ed9423545fd
jsbravo-sw <js.bravo@uniandes.edu.co>
2018-10-02 15:18:51 -0500

    Corrección merge estante, bodega

--- It1_B-05_js.bravo_je.cantor/Java/src/uniandes/isis2304/superAndes/negocio/Bodega.java
+++ It1_B-05_js.bravo_je.cantor/Java/src/uniandes/isis2304/superAndes/negocio/Bodega.java
@@ -1,40 +1,36 @@
 package uniandes.isis2304.superAndes.negocio;
 
-<<<<<<< HEAD
 /**
  * Clase para modelar el concepto BODEGA del negocio de SuperAndes.
  *
- * @author Juan Sebastián Bravo
+ * @author Juan Sebastiï¿½n Bravo
  */
 public class Bodega extends Almacenamiento {
-=======
-public class Bodega extends Almacenamiento{
->>>>>>> be693d3685fb54ba130e13d7115c6e5f543723bc
-	
+
 	/* ****************************************************************
-	 * 			Métodos 
+	 * 			Mï¿½todos
 	 *****************************************************************/
-	
+
 	/**
-     * Constructor sin parámetros por defecto
+     * Constructor sin parï¿½metros por defecto
      */
 	public Bodega()
 	{
 		super();
 	}
-	
+
 	/**
 	 * Constructor con valores.
 	 * @param idAlmacenamiento - El id de la bodega.
 	 * @param cantidadProductos - La cantidad de productos que posee la bodega.
-	 * @param capacidadPeso - El peso máximo que soporta la bodega en gr.
-	 * @param capacidadVolumen - El volumen máximo que soporta la bodega en cm3.
+	 * @param capacidadPeso - El peso mï¿½ximo que soporta la bodega en gr.
+	 * @param capacidadVolumen - El volumen mï¿½ximo que soporta la bodega en cm3.
 	 */
-	
+
 	public Bodega (long idAlmacenamiento, int cantidadProductos, double capacidadPeso, double capacidadVolumen)
 	{
 		super(idAlmacenamiento, cantidadProductos, capacidadPeso, capacidadVolumen);
 	}
-	
-           
+
+
 }
--- It1_B-05_js.bravo_je.cantor/Java/src/uniandes/isis2304/superAndes/negocio/Estante.java
+++ It1_B-05_js.bravo_je.cantor/Java/src/uniandes/isis2304/superAndes/negocio/Estante.java
@@ -1,35 +1,26 @@
 package uniandes.isis2304.superAndes.negocio;
 
-<<<<<<< HEAD
 /**
  * Clase para modelar el concepto ESTANTE del negocio de SuperAndes.
  *
- * @author Juan Sebastián Bravo
+ * @author Juan Sebastiï¿½n Bravo
  */
 public class Estante extends Almacenamiento {
-=======
-public class Estante extends Almacenamiento{
-	
-	private Long estanteId;
-	
-	private String tipoEstante;
->>>>>>> be693d3685fb54ba130e13d7115c6e5f543723bc
-	
+
 	/**
 	 * El nivel de abastecimiento que tiene un estante de los productos.
 	 */
 	private double nivelDeAbastecimiento;
-	
+
 	/* ****************************************************************
-	 * 			Métodos 
+	 * 			Mï¿½todos
 	 *****************************************************************/
-	
+
 	 /**
-     * Constructor sin parámetros por defecto
+     * Constructor sin parï¿½metros por defecto
      */
 	public Estante()
 	{
-<<<<<<< HEAD
 		super();
 		setNivelDeAbastecimiento(0.0);
 	}
@@ -37,8 +28,8 @@ public class Estante extends Almacenamiento{
 	 * Constructor con valores.
 	 * @param idAlmacenamiento - El id del estante.
 	 * @param cantidadProductos - La cantidad de productos que posee el estante.
-	 * @param capacidadPeso - El peso máximo que soporta el estante en gr.
-	 * @param capacidadVolumen - El volumen máximo que soporta el estante en cm3.
+	 * @param capacidadPeso - El peso mï¿½ximo que soporta el estante en gr.
+	 * @param capacidadVolumen - El volumen mï¿½ximo que soporta el estante en cm3.
 	 * @param nivelDeAbastecimiento - El nivel de abastecimiento del producto que tiene el estante.
 	 */
 	public Estante(long idAlmacenamiento, int cantidadProductos, double capacidadPeso, double capacidadVolumen, double nivelDeAbastecimiento)
@@ -46,10 +37,10 @@ public class Estante extends Almacenamiento{
 		super(idAlmacenamiento, cantidadProductos, capacidadPeso, capacidadVolumen);
 		setNivelDeAbastecimiento(nivelDeAbastecimiento);
 	}
-	
+
 	/**
      * Retorna el nivel de abastecimiento del estante.
-     * 
+     *
    	 * @return El id del abastecimiento.
    	 */
 	public double getNivelDeAbastecimiento() {
@@ -57,40 +48,11 @@ public class Estante extends Almacenamiento{
 	}
 	/**
      * Asigna el nivel de abastecimiento del estante.
-     * 
+     *
    	 * @param nivelDeAbastecimiento -  El nivel de abastecimiento del estante.
    	 */
 	public void setNivelDeAbastecimiento(double nivelDeAbastecimiento) {
 		this.nivelDeAbastecimiento = nivelDeAbastecimiento;
 	}
-=======
-		estanteId = 0L;
-		tipoEstante = "";
-		abastecimiento = 0.0;
-		capacidadPeso = 0.0;
-		cantidadProductos = 0;
-	}
-	
-	public Estante(double abastecimiento, double capacidadPeso, int cantidadProductos, String tipoEstante)
-	{
-		this.estanteId = 0L;
-		this.tipoEstante = tipoEstante;
-		this.abastecimiento = abastecimiento;
-		this.capacidadPeso = capacidadPeso;
-		this.cantidadProductos = cantidadProductos;
-	}
-	
-	public Long getEstanteId(){return estanteId;}
-	public String getTipoEstante(){return tipoEstante;}
-	public double getCapacidadVolumen(){return abastecimiento;}
-	public double getCapacidadPeso(){return capacidadPeso;}
-	public double getCapacidadProductos(){return cantidadProductos;}
-	
-	public void setEstanteId(Long estanteId){this.estanteId = estanteId;}
-	public void setTipoEstante(String tipoEstante){this.tipoEstante = tipoEstante;}
-	public void setCapacidadVolumen(double capacidadVolumen){this.abastecimiento = capacidadVolumen;}
-	public void setCapacidadPeso(double capacidadPeso){this.capacidadPeso = capacidadPeso;}
-	public void setCapacidadProductos(int cantidadProductos){this.cantidadProductos = cantidadProductos;}
->>>>>>> be693d3685fb54ba130e13d7115c6e5f543723bc
 
 }
